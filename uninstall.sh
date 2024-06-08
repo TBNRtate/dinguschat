7b5107ef10b028169b1f33adcf7197c871fc7cbd
TBNRtate <93158866+TBNRtate@users.noreply.github.com>
2024-06-08 13:58:55 -0600

    Delete uninstall.sh

--- uninstall.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-ssh root@app.gaevoy.com 'bash -s' <<'ENDSSH'
-systemctl stop GaevCryptoChat
-systemctl disable GaevCryptoChat 
-rm /etc/systemd/system/GaevCryptoChat.service 
-systemctl daemon-reload
-systemctl reset-failed
-rm -rf /apps/GaevCryptoChat
-ENDSSH
