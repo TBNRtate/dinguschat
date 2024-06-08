1fc06698cd07ec260a1e04da14217a546a1e9bbf
TBNRtate <93158866+TBNRtate@users.noreply.github.com>
2024-06-08 14:00:52 -0600

    Add files via upload

---
+++ Gaev.Chat/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Gaev.Chat
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services) =>
+            services.AddMvc();
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env) =>
+            app
+                .UseMvc()
+                .UseDefaultFiles()
+                .UseStaticFiles();
+    }
+}
